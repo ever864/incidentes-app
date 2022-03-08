621a40d8569bf7a6b2c07d50bef58cceec31ac21
ever <ever864@gmail.com>
2022-03-08 16:31:34 -0500

    Se refactoriza las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'incident/index'
-  get 'incident/create'
-  get 'incident/new'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
-
+  
+  devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :incident, only: [:new, :create, :show, :index]
 end
