e15925fd46d545d51b76caf84751a967d3df52fe
ever <ever864@gmail.com>
2022-03-10 15:52:34 -0500

    Se agrega un input para las imagenes en el formulario con active storage

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,7 +1,8 @@
 class Incident < ApplicationRecord
+  has_one_attached :image_incident
   has_rich_text :descripcion
 
-  validates :asunto, :descripcion, presence: true
+  validates :asunto, :descripcion, :image_incident, presence: true
   # Associations
   belongs_to :user
 end
